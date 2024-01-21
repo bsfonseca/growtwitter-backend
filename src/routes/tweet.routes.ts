008b9e964ed0c687480b85fc5e8a6e84cad3b115
bsfonseca <bs.fonseca12@gmail.com>
2024-01-21 14:35:55 -0300

    fix: adição mergeparams na rota tweet

--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { TweetController } from "../controllers/tweet.controller";
+import { validaLoginMiddleware } from "../middlewares/login.middleware";
 
 export function tweetRoutes() {
-    const router = Router();
+    const router = Router({
+        mergeParams: true,
+    });
 
     const tweetController = new TweetController();
 
-    router.post("/", tweetController.criarTweet);
-    router.get("/", tweetController.listarTweets);
-    router.get("/:idTweet", tweetController.obterTweet);
+    router.post("/", [validaLoginMiddleware], tweetController.criarTweet);
+    router.get("/", [validaLoginMiddleware], tweetController.listarTweets);
+    router.get("/:idTweet", [validaLoginMiddleware], tweetController.obterTweet);
 
     return router;
 }

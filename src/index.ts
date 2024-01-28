45e12d295e8954c527f4bcb63728b92816f603f0
bsfonseca <bs.fonseca12@gmail.com>
2024-01-28 13:20:08 -0300

    chore: instalação biblioteca cors

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import express from "express";
+import cors from "cors";
 
 import { usuarioRoutes } from "./routes/usuario.routes";
 import { tweetRoutes } from "./routes/tweet.routes";
 import { loginRouter } from "./routes/login.routes";
 import { likeRouter } from "./routes/like.routes";
+import { TweetController } from "./controllers/tweet.controller";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+const tweetController = new TweetController();
 
 app.use("/usuario", usuarioRoutes());
 app.use("/usuario/:id/tweet", tweetRoutes());
 app.use("/login", loginRouter());
 app.use("/usuario/:id/tweet/:idTweet/like", likeRouter());
+
+app.get("/tweets", tweetController.listarTodosTweets);
 
 app.listen(3333, () => {
     console.log("A API está rodando!");
